df69a524d2b1f072d8517ab0681ca51de48e53ad
Scott Allen <scott@OdeToCode.com>
2012-12-23 16:20:45 -0500

    not sure

--- OdeToFood/Controllers/AccountController.cs
+++ OdeToFood/Controllers/AccountController.cs
@@ -2,13 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Transactions;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
 using DotNetOpenAuth.AspNet;
 using Microsoft.Web.WebPages.OAuth;
 using WebMatrix.WebData;
-using OdeToFood.Filters;
 using OdeToFood.Models;
 
 namespace OdeToFood.Controllers
